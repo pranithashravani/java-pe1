c985464136c891a3e0eadf694b51671f2f6c7d38
pranithashravani <masamsetty.pranithashravani@gmail.com>
2019-06-28 12:21:41 +0530

    Update DescendingTest.java

--- src/main/test/com/stackroute/junitdemo/DescendingTest.java
+++ src/main/test/com/stackroute/junitdemo/DescendingTest.java
@@ -10,35 +10,35 @@ import static org.junit.Assert.*;
 
 
     public class DescendingTest {
-        Descending sn;
+        Descending object;
         @Before
         public void set(){
-            sn=new Descending();
+            object=new Descending();
         }
         @After
         public void tear(){
-            sn=null;
+            object=null;
         }
         @Test
         public void givenNumbershouldReturnFalseMessage(){
-            String result=sn.sortTheNumber(2345);
+            String result=object.sortTheNumber(2345);
             assertEquals("False",result);
         }
 
         @Test
         public void givenNumbershouldReturnTrueMessage(){
-            String result=sn.sortTheNumber(88888);
+            String result=object.sortTheNumber(88888);
             assertEquals("True",result);
         }
         @Test
         public void givenZeroshouldReturnMessage(){
-            String result=sn.sortTheNumber(0);
+            String result=object.sortTheNumber(0);
             assertEquals("False",result);
         }
 
         @Test
         public void givenNeagitiveValueshouldReturnMessage(){
-            String result=sn.sortTheNumber(-2345);
+            String result=object.sortTheNumber(-2345);
             assertEquals("False",result);
         }
     }
